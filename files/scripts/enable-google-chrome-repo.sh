7ae97d0e2facd4bac82d66ee15abf82f73687f70
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-12-19 12:27:16 -0500

    update how we enable chrome repo

--- files/scripts/enable-google-chrome-repo.sh
+++ files/scripts/enable-google-chrome-repo.sh
@@ -2,12 +2,5 @@
 
 set -e
 
-# dnf config-manager setopt google-chrome.enabled=1
-
-# Enable the Google Chrome repo by setting enabled=1
-if [ -f /etc/yum.repos.d/google-chrome.repo ]; then
-  sudo sed -i 's/^enabled=0$/enabled=1/' /etc/yum.repos.d/google-chrome.repo
-  echo "Google Chrome repository enabled."
-else
-  echo "/etc/yum.repos.d/google-chrome.repo not found."
-fi
+dnf install -y fedora-workstation-repositories
+dnf config-manager setopt google-chrome.enabled=1
